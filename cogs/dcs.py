7ef5f1e5a49cf88bf476a1c4b4347bf56a80fe5e
nboughton83 <nboughton+github@pm.me>
2020-10-06 17:25:02 -0400

    removed old comments

--- cogs/dcs.py
+++ cogs/dcs.py
@@ -43,44 +43,5 @@ class DCS(commands.Cog, name="dcs"):
                 await self.bot.change_presence(activity=discord.Game(game))
 
 
-'''
-    async def set_presence(self, status, server_key):
-        await self.bot.wait_until_ready()
-        server_data = self.key_data[server_key]
-        game="{} players on {} playing on {}".format(status["players"], server_data["alias"], status["missionName"])
-        health=self.determine_health(status, server_key)
-        bot_status=discord.Status.online
-        if health.status == "Unhealthy":
-            bot_status=discord.Status.idle
-            game="Slow updates - " + game
-        elif health.status == "Offline":
-            bot_status=discord.Status.dnd
-            game="{} Server offline".format(server_data["alias"])
-        await self.bot.change_presence(status=bot_status, game=discord.Game(name=game))
-
-    async def get_status(self, key):
-        url = self.base_url + key
-        resp = await self.session.get(url)
-        if (resp.status != 200):
-            raise ErrorGettingStatus(resp.status)
-        status = json.loads(await resp.text())
-        #Hoggy Server counts himself among the players.
-        status["players"] = status["players"] - 1
-        status["maxPlayers"] = status["maxPlayers"] - 1
-        return status
-
-    def determine_health(self, status, server_key):
-        last_update = arrow.get(status["updateTime"])
-        return ServerHealth(last_update, server_key)
-
-    def humanize_time(self, updateTime):
-        arrowtime = arrow.get(updateTime)
-        return arrowtime.humanize()
-
-    def get_mission_time(self, status):
-        time_seconds = datetime.timedelta(seconds=status["data"]["uptime"])
-        return str(time_seconds).split(".")[0]
-'''
-
 def setup(bot):
     bot.add_cog(DCS(bot))
